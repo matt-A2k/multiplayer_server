64c2e3b38e1314ad86ff5fd2cfe1988945be9fe2
matt-A2k <113608027+matt-A2k@users.noreply.github.com>
2025-08-30 22:59:00 -0400

    web sockets

--- app/main.py
+++ app/main.py
@@ -1,7 +1,69 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, WebSocket
+from starlette.middleware.cors import CORSMiddleware
+from starlette.websockets import WebSocketState
+import asyncio
+import json
+import time
 
 app = FastAPI()
 
-@app.get("/", response_class=HTMLResponse)
-async def read_root():
-    return "<center><h1>hello world</h1></center>"
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"]
+)
+
+@app.get("/debug")
+async def debug():
+    return "<center>hello world</center>"
+
+@app.get('/view/{game}')
+async def test(game:str):
+    if not game in games.keys():return "No Connections"
+    return(games[game]['players'])
+
+games = {}
+
+@app.websocket("/0/{game_name}/{player_name}/{game_password}")
+async def game_socket(sock:WebSocket, game_name:str, player_name:str, game_password:str):
+
+    disconnect = False
+
+    # perform checks before attempting to connect
+    if(game_name in games):
+        if(games[game_name]['password']!=game_password):
+            await sock.close(reason='incorrect password to existing game')
+#            print('incorrect password')
+            disconnect = True
+        elif(player_name in games[game_name]['players']):
+            await sock.close(reason='player name already exists')
+#            print('name exists')
+            disconnect = True
+    else:games[game_name] = {'password':game_password, 'player_send':{}, 'players':{}}
+
+    await sock.accept()
+    if(disconnect):return
+#       print(f"{player_name} joined {game_name}")
+
+    games[game_name]['players'][player_name] = ''
+    games[game_name]['player_send'][player_name] = asyncio.Event()
+
+    async def send():
+        while game_name in games:
+            await games[game_name]['player_send'][player_name].wait()
+            await sock.send_text(json.dumps(games[game_name]['players']))
+            games[game_name]['player_send'][player_name].clear()
+
+    async def recv():
+        while True:
+            data = await sock.receive_text()
+            if(data!=''):games[game_name]['players'][player_name]=data
+            for player in games[game_name]['player_send'].keys():
+                games[game_name]['player_send'][player].set()
+
+    try:
+        # main code that gets run in a loop if everything above works sucessfully
+        await asyncio.gather(recv(), send())
+    except Exception as e:
+        del games[game_name]['players'][player_name]
+        del games[game_name]['player_send'][player_name]
+        if(len(games[game_name]['players'])==0):del games[game_name]
