c17029c41160417516b8b4b3cdf490ff51fcd6f8
matt-A2k <113608027+matt-A2k@users.noreply.github.com>
2025-08-30 22:54:36 -0400

    test2

--- app/main.py
+++ app/main.py
@@ -2,6 +2,6 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")
+@app.get("/", response_class=HTMLResponse)
 async def read_root():
     return "<center><h1>hello world</h1></center>"
