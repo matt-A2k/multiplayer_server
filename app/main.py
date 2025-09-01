3e8f7c6239c7031cc9cbe7eb6cfe916ec610181e
matt-A2k <113608027+matt-A2k@users.noreply.github.com>
2025-08-31 20:09:08 -0400

    Update main.py

--- app/main.py
+++ app/main.py
@@ -12,18 +12,17 @@ app.add_middleware(
     allow_origins=["*"]
 )
 
+data = ""
+
 @app.get("/debug")
 async def debug():
-    return "<center>hello world</center>"
+    return data
 
 @app.websocket("/socket/")
 async def sock(sock:WebSocket):
     await sock.accept()
 
     while True:
-        print('a')
         data = await sock.receive_text()
-        print('b')
         await sock.send_text(f"got: {data}")
-        print('c')
         print(data)
